017b6cdc8c38ff55d26c53046adde9af98a75de2
Piotr <piotrek124@hotmail.com>
2025-03-24 18:48:15 +0100

    check for frame success

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import cv2
 import paho.mqtt.client as mqtt
 import numpy as np
 
-
-
 def on_connect(client, userdata, flags, rc, properties=None):
     print(f"Connected with result code: {rc}")
-
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
@@ -17,7 +14,6 @@ client.on_message = on_message
 
 client.username_pw_set(username="verification_node", password="admin")
 client.connect("accesscontrol.home", 1883)
-# client.connect("localhost", 1883)
 
 cap = cv2.VideoCapture(0)
 
@@ -27,7 +23,11 @@ while True:
         print("Failed to grab frame.")
         break
 
-    _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+    success, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
 
-    client.publish("webcam/feed", buffer.tobytes())
+    if success:
+        client.publish("webcam/feed", buffer.tobytes())
+    else:
+        print("Failed to encode frame.")
+
     client.loop()
